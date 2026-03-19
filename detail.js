11e0b7d9854f0bfa52088630c9cfa7eec48916b0
Abishek Sriram G <abisheksriram2001@gmail.com>
2026-03-19 22:55:04 +0530

    Update detail.js

--- detail.js
+++ detail.js
@@ -1,1 +1,32 @@
 
+const params = new URLSearchParams(window.location.search);
+const plant = params.get("plant");
+const id = params.get("id");
+
+fetch('data.json')
+  .then(res => res.json())
+  .then(data => {
+
+    let result;
+
+    if (id) {
+      result = data.find(d => d.id === id);
+    } else if (plant) {
+      result = data.find(d => d.plant === plant);
+    }
+
+    const container = document.getElementById("details");
+
+    if (result) {
+      container.innerHTML = `
+        <p><b>Plant:</b> ${result.plant}</p>
+        <p><b>Part:</b> ${result.part}</p>
+        <p><b>ID:</b> ${result.id}</p>
+        <p><b>Name:</b> ${result.name}</p>
+        <p><b>Reference:</b> ${result.reference}</p>
+      `;
+    } else {
+      container.innerHTML = "No data found";
+    }
+
+  });
